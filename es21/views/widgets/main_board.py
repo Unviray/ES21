ab7fa939eb725c6422d4a7dd509ff982f23856e4
Unviray <unviray@gmail.com>
2020-04-30 13:54:01 +0300

    format phone input

--- es21/views/widgets/main_board.py
+++ es21/views/widgets/main_board.py
@@ -5,11 +5,7 @@ es21.views.widgets.main_board
 
 from ...utils import templated
 
-from markupsafe import Markup
-
 
 @templated('widgets/main_board.html')
 def entry(content):
-    content = Markup(content).unescape()
-
     return dict(content=content)
