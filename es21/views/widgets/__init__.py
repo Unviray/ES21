6ae81a89865d6e6e1eb5982ae32984b9f1575693
Unviray <unviray@gmail.com>
2020-05-12 12:12:29 +0300

    retrive content of search result with ajax

--- es21/views/widgets/__init__.py
+++ es21/views/widgets/__init__.py
@@ -19,11 +19,11 @@ blueprint = Blueprint('widget', __name__, url_prefix='/widget')
 
 
 blueprint.add_url_rule(
-    '/pr_card',
+    '/pr_card/<int:id>',
     'pr_card',
     pr_card.entry, )
 blueprint.add_url_rule(
-    '/pr_card_uncached',
+    '/pr_card_uncached/<int:id>',
     'pr_card_uncached',
     pr_card_uncached.entry, )
 
